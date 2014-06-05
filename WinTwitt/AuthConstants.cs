14b7560e96c1a4ce67da2d53df35921878d36b0f
albazamanillo <albazamanillo@gmail.com>
2014-06-05 23:55:05 +0200

    Initial Commit

---
+++ WinTwitt/AuthConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTwitt
+{
+    public static class AuthConstants
+    {
+        public const string ConsumerKey = "CNLcpJ0WVcTki5iqUAsE363yJ";
+        public const string ConsumerSecret = "RsdC8KKA4wlSujjfeZ20wO0uNzXc4EIWUjZ68vE8sPvpUpplG7";
+    }
+}
---
+++ WinTwitt/UrlConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTwitt
+{
+     public static class UrlConstants
+    {
+        public const string RequestToken = "https://api.twitter.com/oauth/request_token";
+        public const string AccessToken = "https://api.twitter.com/oauth/authorize";
+    }
+}
+}
